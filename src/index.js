d53bc5ca5aaa26bc852b645082295ce1bbdb6940
unknown <jeonsubin5156@inha.edu>
2024-07-14 16:00:38 +0900

    feat:NavBar and Footer update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import NotFound from './pages/NotFound';
+import CreatePost from './pages/CreatePost';
+import EditProfile from './pages/EditProfile';
+import LoginForm from './pages/LoginForm';
+import Post from './pages/Post';
+import PostList from './pages/PostList';
+import RegisterForm from './pages/RegisterForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<LoginForm />} />
+          <Route path="CreatePost" element={<CreatePost />} />
+          <Route path="EditProfile" element={<EditProfile />} />
+          <Route path="Post" element={<Post />} />
+          <Route path="PostList" element={<PostList />} />
+          <Route path="RegisterForm" element={<RegisterForm />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
