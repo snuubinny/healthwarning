cc0d8fd7dc88fdcac4ee49469700cc40f7ca515e
unknown <jeonsubin5156@inha.edu>
2024-07-27 01:43:41 +0900

    feat/post_box-add

--- src/components/AchievementRate.jsx
+++ src/components/AchievementRate.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import SleepImage from "../img/sleep.png";
+import MealImage from "../img/meal.png";
+import MedicineImage from "../img/medicine.png";
+import ExerciseImage from "../img/exercise.png";
+import CatImage from "../img/cat.png";
 
 const Wrapper = styled.div`
   display: flex;
@@ -19,18 +24,23 @@ const PercentContainer = styled.div`
   box-sizing: border-box;
   cursor: pointer;
   height: 450px;
-  margin-bottom: 20px;
+  margin-bottom: 50px;
   margin-top: 50px;
-  &:hover {
-    border: 1px solid #ff832b;
-  }
+`;
+
+const PercentText = styled.p`
+  text-align: center;
+  font-weight: bold;
+  font-size: 20px;
+  margin-top: 20px;
+  line-height: 2;
 `;
 
 const TextWrap = styled.p`
   text-align: center;
   font-weight: bold;
-  font-size: 18px;
-  margin-top: 30px;
+  font-size: 15px;
+  margin-top: 10px;
   line-height: 2;
 `;
 
@@ -38,7 +48,7 @@ const Highlight = styled.span`
   color: #ff832b;
 `;
 
-const ContentContainer = styled.div`
+const DiaryContainer = styled.div`
   width: 50%;
   padding: 10px;
   position: relative;
@@ -48,49 +58,175 @@ const ContentContainer = styled.div`
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   box-sizing: border-box;
   cursor: pointer;
-  height: 300px;
+  min-height: 400px;
   margin-bottom: 20px;
+  margin-top: 50px;
 `;
-
+const StyledCatImage = styled.img`
+  width: 110px;
+  height: auto;
+  border-radius: 8px;
+  position: absolute;
+  right: 0px;
+  bottom: 0px;
+`;
 const CardImage = styled.img`
   width: 250px;
   height: auto;
   border-radius: 8px;
   margin-left: 240px;
-  margin-top: 40px;
+  margin-top: 70px;
 `;
+
+const Header = styled.div`
+  width: 100%;
+  height: 60px;
+  background: linear-gradient(to right, #ff832b, #ffb74d);
+  border-top-left-radius: 12px;
+  border-top-right-radius: 12px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const HeaderText = styled.p`
+  text-align: left;
+  font-weight: bold;
+  font-size: 18px;
+  color: white;
+  margin-top: 12px;
+  margin-left: 15px;
+  line-height: 2;
+`;
+
+const DetailTitle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 750px;
+  height: 60px;
+  border-radius: 20px;
+  margin: 10px;
+  font-size: 22px;
+  background: linear-gradient(to right, #ff832b, #ffb74d);
+  color: white;
+  font-weight: bold;
+`;
+
+const DetailBoxWrap = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 15px;
+  justify-items: center;
+  margin-top: 30px;
+`;
+
+const Box = styled.div`
+  width: 170px;
+  padding: 10px;
+  position: relative;
+  border-radius: 12px;
+  background-color: #ffffff;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  height: 180px;
+  margin-bottom: 20px;
+  &:hover {
+    border: 1px solid #ff832b;
+  }
+`;
+
+const BoxImage1 = styled.img`
+  width: 120px;
+  height: auto;
+  border-radius: 8px;
+  margin-left: 25px;
+  margin-top: 15px;
+`;
+
+const BoxImage2 = styled.img`
+  width: 100px;
+  height: auto;
+  border-radius: 8px;
+  margin-left: 35px;
+  margin-top: 10px;
+`;
+
+const BoxImage3 = styled.img`
+  width: 110px;
+  height: auto;
+  border-radius: 8px;
+  margin-left: 35px;
+  margin-top: 27px;
+`;
+const BoxImage4 = styled.img`
+  width: 90px;
+  height: auto;
+  border-radius: 8px;
+  margin-left: 35px;
+  margin-top: 10px;
+`;
+
+const SleepBox = styled(Box)``;
+const MedicineBox = styled(Box)``;
+const ExerciseBox = styled(Box)``;
+const MealBox = styled(Box)``;
 
 const AchievementRate = () => {
   const percentage = 75;
-  const goalSleep = 8;
-  const actualSleep = 6;
-  const goalGym = 50;
-  const acualGym = 30;
 
   return (
     <Wrapper>
       <PercentContainer>
+        <Header>
+          <HeaderText>오늘의 달성률</HeaderText>
+        </Header>
         <CardImage
           src={`${process.env.PUBLIC_URL}/percentcircle.png`}
           alt="Example"
         />
-
-        <TextWrap>
+        <PercentText>
           잘하고 있어요! 목표의 <Highlight>{percentage}%</Highlight>를
           달성했어요!
-        </TextWrap>
+        </PercentText>
       </PercentContainer>
-      <ContentContainer>
-        <TextWrap>
-          목표수면시간 {goalSleep}시간 중 <Highlight>{actualSleep}</Highlight>
-          시간 수면하였습니다.
-          <br />
-          목표운동시간 {goalGym}분 중 <Highlight>{acualGym}</Highlight>분
-          운동했습니다.
-          <br />
-          등등의 내용들...
-        </TextWrap>
-      </ContentContainer>
+      <DetailTitle>세부 달성률</DetailTitle>
+      <DetailBoxWrap>
+        <SleepBox>
+          <BoxImage1 src={SleepImage} alt="Sleep" />
+          <TextWrap>
+            목표 수면시간 <Highlight>8시간</Highlight> 중<br />
+            <Highlight>6시간 </Highlight>
+            수면하였습니다.
+          </TextWrap>
+        </SleepBox>
+        <MedicineBox>
+          <BoxImage2 src={MedicineImage} alt="Medicine" />
+          <TextWrap>
+            목표 약복용 <Highlight>3회 </Highlight>중<br />
+            <Highlight>3회 </Highlight> 복용하였습니다.
+          </TextWrap>
+        </MedicineBox>
+        <ExerciseBox>
+          <BoxImage3 src={ExerciseImage} alt="Exercise" />
+          <TextWrap>
+            목표 운동시간 <Highlight>30분 </Highlight>중<br />
+            <Highlight>40분 </Highlight> 운동하였습니다.
+          </TextWrap>
+        </ExerciseBox>
+        <MealBox>
+          <BoxImage4 src={MealImage} alt="Meal" />
+          <TextWrap>
+            목표 식사횟수 <Highlight>3끼 </Highlight>중<br />
+            <Highlight>3끼 </Highlight> 섭취하였습니다.
+          </TextWrap>
+        </MealBox>
+      </DetailBoxWrap>
+      <DiaryContainer>
+        <Header>
+          <HeaderText>오늘의 일기</HeaderText>
+        </Header>
+        <StyledCatImage src={CatImage} alt="Catimage" />
+      </DiaryContainer>
     </Wrapper>
   );
 };
