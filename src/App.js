a1c7dc33008234b014a81ed571b56ccfc4ac7990
unknown <jeonsubin5156@inha.edu>
2024-08-22 15:29:39 +0900

    App_ver

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 import "./App.css";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  max-width: 420px;
+  margin: auto;
+
+  @media (min-width: 421px) {
+    box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.1); /* 0.1의 투명도를 가진 얇은 선 */
+  }
+`;
 
 function App() {
   const location = useLocation();
@@ -10,12 +20,14 @@ function App() {
 
   return (
     <>
-      {!isMainPage && <NavBar />}
-      <ScrollToTop />
-      <div className="outlet-background">
-        <Outlet />
-      </div>
-      {!isMainPage && <Footer />}
+      <AppWrapper>
+        {!isMainPage && <NavBar />}
+        <ScrollToTop />
+        <div className="outlet-background">
+          <Outlet />
+        </div>
+        {!isMainPage && <Footer />}
+      </AppWrapper>
     </>
   );
 }
